5c99bfdbefa9c360dc1c9144c52dff1a3dc062dd
SajjadSj66 <sajjadjadidi666@gmail.com>
2026-08-22 20:19:58 +0330

    add
    add

--- aras/settings.py
+++ aras/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.1/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/6.1/ref/settings/
+https://docs.djangoproject.com/en/6.1/ref/settings/ 
 """
 
 from pathlib import Path
@@ -19,12 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
+import os
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ix06j$z49now3gx^@r#a364nc+x)9p1ir1x1iy7s_3ljp+im$q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     ".onrender.com",
